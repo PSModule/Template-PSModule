5ca107430de89057aba2ce1108bb4f6e9358533a
Marius Storhaug <marstor@hotmail.com>
2024-03-28 22:12:45 +0100

    align

--- tests/PSModule.Tests.ps1
+++ tests/PSModule.Tests.ps1
@@ -1,11 +1,18 @@
-﻿Describe 'PSModule' {
+﻿[CmdletBinding()]
+Param(
+    # Path to the module to test.
+    [Parameter()]
+    [string] $Path
+)
+
+Describe 'PSModule' {
     Context 'Module' {
         It 'The module should be available' {
             Get-Module -Name 'PSModule' -ListAvailable | Should -Not -BeNullOrEmpty
             Write-Verbose (Get-Module -Name 'PSModule' -ListAvailable | Out-String) -Verbose
         }
         It 'The module should be importable' {
-            { Import-Module -Name 'PSModule' -Verbose -Force } | Should -Not -Throw
+            { Import-Module -Name 'PSModule' -Verbose -RequiredVersion 999.0.0 -Force } | Should -Not -Throw
         }
     }
 
