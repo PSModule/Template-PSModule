c16f84465cd7793ccdd59f97f3b75d1db348f41a
Marius Storhaug <marstor@hotmail.com>
2024-04-14 00:58:19 +0200

    Align with Test-PSModule

--- tests/Module.Tests.ps1
+++ tests/Module.Tests.ps1
@@ -7,18 +7,8 @@ Param(
 
 Write-Verbose "Path to the module: [$Path]" -Verbose
 
-Describe 'PSModuleTemplate' {
-    Context 'Module' {
-        It 'The module should be available' {
-            Get-Module -Name 'PSModuleTemplate' -ListAvailable | Should -Not -BeNullOrEmpty
-            Write-Verbose (Get-Module -Name 'PSModuleTemplate' -ListAvailable | Out-String) -Verbose
-        }
-        It 'The module should be importable' {
-            { Import-Module -Name 'PSModuleTemplate' -Verbose -RequiredVersion 999.0.0 -Force } | Should -Not -Throw
-        }
-    }
-
-    Context "Function 'Test-PSModuleTemplate'" {
+Describe 'Module' {
+    Context "Function: 'Test-PSModuleTemplate'" {
         It 'Should be able to call the function' {
             Test-PSModuleTemplate -Name 'World' | Should -Be 'Hello, World!'
             Write-Verbose (Test-PSModuleTemplate -Name 'World' | Out-String) -Verbose
