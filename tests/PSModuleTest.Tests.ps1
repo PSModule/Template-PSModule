5ea754e8cbdca498d1d8432c3468d033fd058ce3
Marius Storhaug <marstor@hotmail.com>
2025-02-18 18:08:36 +0100

    🩹 [Patch]: Update .gitignore for .Net build outputs, add jscpd configuration, modify license year, and enhance linter settings

--- tests/PSModuleTest.Tests.ps1
+++ tests/PSModuleTest.Tests.ps1
@@ -1,4 +1,15 @@
-﻿Describe 'Module' {
+﻿[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSReviewUnusedParameter', '',
+    Justification = 'Required for Pester tests'
+)]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSUseDeclaredVarsMoreThanAssignments', '',
+    Justification = 'Required for Pester tests'
+)]
+[CmdletBinding()]
+param()
+
+Describe 'Module' {
     It 'Function: Get-PSModuleTest' {
         Get-PSModuleTest -Name 'World' | Should -Be 'Hello, World!'
     }
